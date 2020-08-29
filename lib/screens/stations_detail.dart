1097283ee77d38194087c22f403514a4b4897919
Hendrik Maus <aidentailor@gmail.com>
2020-08-29 20:34:39 +0200

    Add a button to share the station ID
    Throws an exception at the moment.

--- lib/screens/stations_detail.dart
+++ lib/screens/stations_detail.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:openweathermap_stations_api/screens/stations_form.dart';
+import 'package:share/share.dart';
 
 import '../openweathermap_stations_v3.dart';
 
@@ -31,8 +32,33 @@ class _StationsDetailState extends State<StationsDetail> {
       ),
       body: Container(
         padding: EdgeInsets.all(8),
-        child: Text(this.widget.station.id),
+        child: Column(
+          children: [
+            Text(this.widget.station.id),
+            RaisedButton(
+              child: Text('Share ID'),
+              onPressed: () {
+                _onShare(context);
+              },
+            )
+          ],
+        ),
       ),
     );
+  }
+
+  void _onShare(BuildContext context) async {
+    // A builder is used to retrieve the context immediately
+    // surrounding the RaisedButton.
+    //
+    // The context's `findRenderObject` returns the first
+    // RenderObject in its descendent tree when it's not
+    // a RenderObjectWidget. The RaisedButton's RenderObject
+    // has its position and size after it's built.
+    final RenderBox box = context.findRenderObject();
+
+    await Share.share(this.widget.station.id,
+        subject: 'Station ID of ${this.widget.station.name}',
+        sharePositionOrigin: box.localToGlobal(Offset.zero) & box.size);
   }
 }
